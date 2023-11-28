22a4cf829c09f0b2066eed9033a00efecf518d0b
ADMIN <ADMIN@192.168.1.40>
2023-11-28 15:16:17 +0530

    First commit.....

---
+++ src/org/hexa/Test1.java
@@ -0,0 +1,15 @@
+package org.hexa;
+
+public class Test1 {
+
+	public static void main(String[] args) {
+		
+		String tierd = "yes";
+		
+		if (tierd=="yes") {
+			System.out.println("take some food");
+		} else {
+System.out.println("do some work");
+		}
+	}
+}
