aded8efc4c3b60727ad10ed7895b76af7b86413f
ADMIN <ADMIN@192.168.1.41>
2023-12-01 13:09:41 +0530

    committing Test2 to BranchX

--- src/org/hexa/Test2.java
+++ src/org/hexa/Test2.java
@@ -1,5 +1,14 @@
 package org.hexa;
 
 public class Test2 {
-
+public static void main(String[] args) {
+		
+		String tierd = "yes";
+		
+		if (tierd=="yes") {
+			System.out.println("take some food");
+		} else {
+System.out.println("do some work");
+		}
+	}
 }
